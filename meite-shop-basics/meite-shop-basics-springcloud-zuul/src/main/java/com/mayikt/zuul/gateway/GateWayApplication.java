55f36507827494eadfdf983a2680bff6a8548b51
gituser <gituser@192.168.156.1>
2019-06-08 09:29:07 +0800

    网关配置

--- meite-shop-basics/meite-shop-basics-springcloud-zuul/src/main/java/com/mayikt/zuul/gateway/GateWayApplication.java
+++ meite-shop-basics/meite-shop-basics-springcloud-zuul/src/main/java/com/mayikt/zuul/gateway/GateWayApplication.java
@@ -1,5 +1,10 @@
 package com.mayikt.zuul.gateway;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.spring.annotation.ApolloConfig;
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,24 +25,61 @@ import java.util.List;
 @EnableEurekaClient
 @EnableZuulProxy
 @EnableSwagger2Doc
+@EnableApolloConfig
 public class GateWayApplication {
+
+	// 获取ApolloConfig
+	@ApolloConfig
+	private Config appConfig;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(GateWayApplication.class, args);
 	}
 
-	// 添加文档来源
+	// 添加文档来源  访问swagger-ui页面的时候  每次都会访问以下get方法
 	@Component
 	@Primary
 	class DocumentationConfig implements SwaggerResourcesProvider {
 		@Override
 		public List<SwaggerResource> get() {
-			List resources = new ArrayList<>();
 			// 网关使用服务别名获取远程服务的SwaggerApi
-			resources.add(swaggerResource("app-mayikt-member", "/app-mayikt-member/v2/api-docs", "2.0"));
-			resources.add(swaggerResource("app-mayikt-weixin", "/app-mayikt-weixin/v2/api-docs", "2.0"));
+			return resources();
+		}
+
+		/**
+		 * 从阿波罗服务器中获取resources
+		 *
+		 * @return
+		 */
+		private List<SwaggerResource> resources() {
+
+			List resources = new ArrayList<>();
+			// app-itmayiedu-order
+			// 网关使用服务别名获取远程服务的SwaggerApi
+			String swaggerDocJson = swaggerDocument();
+			JSONArray jsonArray = JSONArray.parseArray(swaggerDocJson);
+			for (Object object : jsonArray) {
+				JSONObject jsonObject = (JSONObject) object;
+				String name = jsonObject.getString("name");
+				String location = jsonObject.getString("location");
+				String version = jsonObject.getString("version");
+				resources.add(swaggerResource(name, location, version));
+			}
 			return resources;
 		}
+
+		/**
+		 * 获取swaggerDocument配置
+		 *
+		 * @return
+		 */
+		private String swaggerDocument() {
+			String property = appConfig.getProperty("mayikt.zuul.swaggerDocument", "");
+			return property;
+		}
+
+
 
 		private SwaggerResource swaggerResource(String name, String location, String version) {
 			SwaggerResource swaggerResource = new SwaggerResource();
